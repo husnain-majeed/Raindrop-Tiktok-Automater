40cda743727d31486673554e3d1ca52d836f5ae3
Husnain Majeed <husnainm3@gmail.com>
2023-02-09 19:48:44 +0000

    Add new empty Program class

---
+++ Raindrop-Tiktok-Automater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raindrop_Tiktok_Automater
+{
+    internal class Program
+    {
+    }
+}
