d0a84ba0bb461433a878ae526213a4b4e68ee37b
Husnain Majeed <husnainm3@gmail.com>
2023-02-09 19:50:48 +0000

    Created Tiktok Video Class

---
+++ Raindrop-Tiktok-Automater/Class1.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Raindrop_Tiktok_Automater
+{
+    public class TiktokVideo
+    {
+        public string VideoUrl { get; set; }
+        public string DownloadUrls { get; set; }
+    }
+}
