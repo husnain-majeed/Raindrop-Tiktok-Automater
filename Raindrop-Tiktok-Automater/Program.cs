4786697fdc5c1c07a0f6c3025cf62f2e906eb476
Husnain Majeed <husnainm3@gmail.com>
2023-02-09 19:22:04 +0000

    Created and tested download links scraper system

--- Raindrop-Tiktok-Automater/Program.cs
+++ Raindrop-Tiktok-Automater/Program.cs
@@ -10,7 +10,7 @@ namespace Raindrop_Tiktok_Automater
         static async Task Main(string[] args)
         {
             const string url = "https://snaptik.app/";
-            string tiktokUrl = "https://www.tiktok.com/@the_sturniolos3/video/7171149384581745922?is_from_webapp=1&sender_device=pc";
+            string tiktokUrl = "https://vm.tiktok.com/ZMYYg2edw/";
 
             using var browserFetcher = new BrowserFetcher();
             await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
@@ -21,7 +21,7 @@ namespace Raindrop_Tiktok_Automater
                 DefaultViewport = null
             });
 
-            var delay = 100;
+            //var delay = 100;
             var page = await browser.NewPageAsync();
             page.Request += Page_Request;
             page.Response += Page_Response;
@@ -29,10 +29,16 @@ namespace Raindrop_Tiktok_Automater
             await page.GoToAsync(url);
 
             var downloadUrlSelector = "#url";
-            var downloadButtonSelector = ".btn-go";
             await page.WaitForSelectorAsync(downloadUrlSelector);
+
+            var downloadButtonSelector = ".btn-go";
             await TypeFieldValue(page, downloadUrlSelector, tiktokUrl);
             await clickButtton(page, downloadButtonSelector);
+
+            var downloadButtonSelector2 = ".btn-main";
+            await page.WaitForSelectorAsync(downloadButtonSelector2);
+            var jsSelectAllAnchors = @"Array.from(document.querySelectorAll('.btn-main')).map(a => a.href);";
+            var urls = await page.EvaluateExpressionAsync<string[]>(jsSelectAllAnchors);
 
         }
          private static async void Page_Response(object sender, ResponseCreatedEventArgs e)
@@ -45,7 +51,6 @@ namespace Raindrop_Tiktok_Automater
             Console.WriteLine(e.Request.ResourceType.ToString());
             Console.WriteLine(e.Request.Url);
         }
-
         private static async Task TypeFieldValue(IPage page, string fieldSelector, string value, int delay = 0)
         {
             await page.FocusAsync(fieldSelector);
