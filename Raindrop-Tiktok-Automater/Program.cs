473e119f40375b19155e1329e2f73ec3d1b83419
Husnain Majeed <husnainm3@gmail.com>
2023-02-25 15:08:46 +0000

    Updated files

--- Raindrop-Tiktok-Automater/Program.cs
+++ Raindrop-Tiktok-Automater/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Raindrop_Tiktok_Automater
+﻿namespace Raindrop_Tiktok_Automater
 {
-    internal class Program
+    class Program
     {
+        static void Main(string[] args)
+        {
+
+        }
     }
 }
